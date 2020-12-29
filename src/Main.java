cfbc6f3fc8faeca6e085d3fce0c5693949ba58e7
elenkalee <elena.lipukhina@gmail.com>
2020-12-29 18:31:19 +0300

    code update

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
         public static void main(String[] args) {
-            long priceInKopek = 22_000_55;
-            int coefficientInKopeck = 20_00;
-            long bonusInMiles = priceInKopek / coefficientInKopeck;
-            System.out.println(bonusInMiles);
+            long price = 22_000_55;
+            int coefficient = 20_00;
+            long miles = price / coefficient;
+            System.out.println(miles);
         }
 }
